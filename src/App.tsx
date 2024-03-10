e66f50f2007d63862c01b1bc07588c6947594858
Milepowe <nilsbitho717@gmail.com>
2024-03-10 12:06:25 +0000

    will move the project to vuejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 "use client";
 
 import React from "react";
-import { Card, Flowbite, Footer } from "flowbite-react";
 import "./index.css";
 import Darkmode from "./random_things/Darkmode";
 
 export function App() {
   return (
     <body>
-      <header className="mx-0 px-0 h-screen">
-        <Darkmode />
-        <h1>random text goes here</h1>
-      </header>
+      <Darkmode />
+      <div className="flex flex-row">
+        <div className="card w-96 bg-base-100 shadow-xl object-center">
+          <div className="card-body ">
+            <h2 className="card-title">cooking very hard</h2>
+            <p>we are still cooking</p>
+          </div>
+          <figure>
+            <img
+              src="https://media1.tenor.com/m/sQOx-qvdkQUAAAAC/malevolent-kitchen-malevolent-shrine.gif"
+              alt="cooking"
+            />
+          </figure>
+        </div>
+      </div>
     </body>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
   daisyui: {
     themes: [
       "light",
-      "dark",
+      "dracula",
     ]
   },
 }
